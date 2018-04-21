dead09f434f0b0146c948659f36b08a91bab547a
pratyush2 <36804395+pratyush2@users.noreply.github.com>
2018-04-21 14:58:10 +0000

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,7 +2,7 @@
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println( "Hello World..!" );
+		System.out.println( "Hello pratyush..!" );
         System.exit( 0 ); //success
 
 
